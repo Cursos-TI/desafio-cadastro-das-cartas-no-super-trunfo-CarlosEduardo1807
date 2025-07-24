6a9985eeee4918103a58cea143ac7b7389d55d1a
carlos <carloseduardosilva.dev@gmail.com>
2025-07-24 20:43:04 -0300

    ajuste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,14 +14,14 @@ int main()
     float area1;
     float pib1;
     int pontosTuristicos1;
-    float densidadePopulacional1 = calcularDensidade(populacao1, area1);
+    float densidadePopulacional1;
 
     // Atributos da cidade 2
     int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
-    float densidadePopulacional2 = calcularDensidade(populacao2, area2);
+    float densidadePopulacional2;
 
     // Entrada de dados - cidade 1
     printf("Digite a população da cidade 1: ");
@@ -49,20 +49,23 @@ int main()
     printf("Digite o número de pontos turísticos da cidade 2: ");
     scanf("%d", &pontosTuristicos2);
 
+    densidadePopulacional1 = calcularDensidade(populacao1, area1);
+    densidadePopulacional2 = calcularDensidade(populacao2, area2);
+
     // Exibição dos dados
     printf("\n========= CARTA DA CIDADE 1 =========\n");
     printf("População: %d habitantes\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: R$ %.2f \n", pib1);
     printf("Pontos turísticos: %d\n", pontosTuristicos1);
-    printf("Densidade: %d\n", densidadePopulacional1);
+    printf("Densidade: %f\n", densidadePopulacional1);
 
     printf("\n========= CARTA DA CIDADE 2 =========\n");
     printf("População: %d habitantes\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f\n", pib2);
     printf("Pontos turísticos: %d\n", pontosTuristicos2);
-    printf("Densidade: %d\n", densidadePopulacional2);
+    printf("Densidade: %f\n", densidadePopulacional2);
 
     return 0;
 }
